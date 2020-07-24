9e767d448e6dd2a66e878f3a750493dc0155c8ae
Filimonov Gleb <filimonovga@live.com>
2020-07-25 01:28:46 +0200

    Structure validation

---
+++ go.mod
@@ -0,0 +1,16 @@
+module our-expenses-server
+
+go 1.14
+
+require (
+	github.com/go-playground/validator/v10 v10.3.0
+	github.com/google/wire v0.4.0
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
+	github.com/joho/godotenv v1.3.0
+	github.com/kisielk/errcheck v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.7
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.6.1
+	go.mongodb.org/mongo-driver v1.3.5
+)
---
+++ validators/validator.go
@@ -0,0 +1,56 @@
+package validators
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+// ValidatorInterface defines validation methods.
+type ValidatorInterface interface {
+	ValidateStruct(object interface{}) []ValidationError
+}
+
+// Validator is a wrapper around "Package validator".
+type Validator struct {
+	*validator.Validate
+}
+
+// ValidationError is a struct with validation error details.
+type ValidationError struct {
+	Field   string `json:"field"`
+	Details string `json:"details"`
+}
+
+var validationErrors = map[string]string{
+	"required": "required, but was not received",
+	"min":      "value or length is less than allowed",
+	"max":      "value or length is bigger than allowed",
+}
+
+// ProvideValidator returns a Validator.
+func ProvideValidator() *Validator {
+	return &Validator{
+		validator.New(),
+	}
+}
+
+// ValidateStruct validates a struct.
+func (v *Validator) ValidateStruct(object interface{}) []ValidationError {
+	validationError := v.Struct(object)
+	var errMsg []ValidationError
+	if validationError != nil {
+		for _, validationError := range validationError.(validator.ValidationErrors) {
+			errMsg = append(errMsg, ValidationError{
+				Field:   validationError.Field(),
+				Details: getValidationMessage(validationError.ActualTag()),
+			})
+		}
+	}
+	return errMsg
+}
+
+func getValidationMessage(s string) string {
+	if v, ok := validationErrors[s]; ok {
+		return v
+	}
+	return s
+}
